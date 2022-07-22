f91a69c636d761b614dd8211baacefaf9c3c4d91
rickie maweu <rickiemaweu@gmail.com>
2022-07-22 14:46:12 +0300

    hook use effect lab

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,27 @@
-import React, { useState } from "react";
+// import useEffect and useCallback//
+import React, { useCallback, useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+
+  useEffect(() => {
+    if (timeRemaining === 0) {
+      setTimeRemaining(10);
+      onAnswered(false);
+    }
+
+    const timerId = setTimeout(() => {
+      setTimeRemaining(timeRemaining - 1)
+    }, 1000);
+
+    return function() {
+      clearTimeout(timerId);
+    };
+  }, [timeRemaining, onAnswered]);
+
+
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
